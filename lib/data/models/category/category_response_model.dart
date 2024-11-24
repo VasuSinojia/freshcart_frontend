70bdb58923fc9d68e83508bc3f66892f9708c2da
Vasu Sinojia <sinojiavasu321@gmail.com>
2024-11-24 21:42:25 +0530

    Updated category_response_model.dart added color_code

--- lib/data/models/category/category_response_model.dart
+++ lib/data/models/category/category_response_model.dart
@@ -6,32 +6,37 @@ class CategoryResponseModel {
   });
 
   CategoryResponseModel.fromJson(Map<String, dynamic> json)
-      : categories = (json['categories'] as List?)?.map((dynamic e) => Categories.fromJson(e as Map<String,dynamic>)).toList();
+      : categories = (json['categories'] as List?)
+            ?.map((dynamic e) => Categories.fromJson(e as Map<String, dynamic>))
+            .toList();
 
-  Map<String, dynamic> toJson() => {
-    'categories' : categories?.map((e) => e.toJson()).toList()
-  };
+  Map<String, dynamic> toJson() =>
+      {'categories': categories?.map((e) => e.toJson()).toList()};
 }
 
 class Categories {
   final int? id;
   final String? name;
   final String? imageUrl;
+  final String? colorCode;
 
   Categories({
     this.id,
     this.name,
     this.imageUrl,
+    this.colorCode,
   });
 
   Categories.fromJson(Map<String, dynamic> json)
       : id = json['id'] as int?,
         name = json['name'] as String?,
-        imageUrl = json['imageUrl'] as String?;
+        imageUrl = json['imageUrl'] as String?,
+        colorCode = json['color_code'] as String?;
 
   Map<String, dynamic> toJson() => {
-    'id' : id,
-    'name' : name,
-    'imageUrl' : imageUrl
-  };
+        'id': id,
+        'name': name,
+        'imageUrl': imageUrl,
+        'color_code': colorCode,
+      };
 }
